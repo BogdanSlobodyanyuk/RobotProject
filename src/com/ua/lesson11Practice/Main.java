ee6b0c06344eb88145fa9af2a20450f68e85f000
Bogdan <bogdan.slobodyanyuk.1998@gmail.com>
2023-02-24 21:33:43 +0000

    Selfwork

---
+++ src/com/ua/lesson11Practice/Main.java
@@ -0,0 +1,24 @@
+package src.com.ua.lesson11Practice;
+
+
+import src.com.ua.lesson11Practice.domain.Employee;
+import src.com.ua.lesson11Practice.service.EmployeeService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EmployeeService employeeService = new EmployeeService();
+        employeeService.printEmployees();
+        Employee employeeWithMaxSalary = employeeService.findEmployeeWithMaxSalary(employeeService.findAll());
+        System.out.println();
+        System.out.println("Employee with max salary: ");
+        employeeService.printEmployee(employeeWithMaxSalary);
+        Employee employeeWithMinSalary = employeeService.findEmployeeWithMinSalary(employeeService.findAll());
+        System.out.println();
+        System.out.println("Employee with min salary: ");
+        employeeService.printEmployee(employeeWithMinSalary);
+        System.out.println();
+        System.out.println("Taxes: " + employeeService.calculateTaxes(employeeService.findAll()));
+    }
+}
+
