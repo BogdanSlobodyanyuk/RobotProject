4376ae1f7f1469026cf50368882d1b40404656a6
Bogdan <bogdan.slobodyanyuk.1998@gmail.com>
2023-03-04 14:18:03 +0000

    Homework lesson 15 (fixed)

--- src/com/ua/lesson15/Main.java
+++ src/com/ua/lesson15/Main.java
@@ -3,8 +3,8 @@ package src.com.ua.lesson15;
 public class Main {
     public static void main(String[] args) {
 
-        Driver driver = new Driver("Bohdan", "Nestorenko");
-        Pilot pilot = new Pilot("Denis","Petrenko");
+        Driveable driver = new Driver("Bohdan", "Nestorenko");
+        Flyeable pilot = new Pilot("Denis","Petrenko");
 
         System.out.println(driver + " always " + driver.drive().concat("s"));
         System.out.println(pilot + " always " + pilot.fly().replace("y","ies"));
