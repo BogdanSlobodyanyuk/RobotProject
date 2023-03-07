6ac96c3f4af162c55c5fec2d22c7ed026dd95278
Bogdan <bogdan.slobodyanyuk.1998@gmail.com>
2023-03-07 23:00:56 +0000

    Homework lesson 17

---
+++ src/com/ua/lesson16/Main.java
@@ -0,0 +1,21 @@
+package src.com.ua.lesson16;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HomeworkService dayFoundByFirstWay = new HomeworkFirstWay();
+        HomeworkService dayFoundBySecondWay = new HomeworkSecondWay();
+        HomeworkService dayFoundByThirdWay = new HomeworkThirdWay();
+
+        System.out.println("Now You are using the First way to find a day for a number");
+        dayFoundByFirstWay.checkHomework();
+        System.out.println("Now You are using the Second way to find a day for a number");
+        dayFoundBySecondWay.checkHomework();
+        System.out.println("Now You are using the Third way to find a day for a number");
+        dayFoundByThirdWay.checkHomework();
+
+
+
+    }
+}
